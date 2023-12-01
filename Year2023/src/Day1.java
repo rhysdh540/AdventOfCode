f0f47856897b339a6071fdbec2472d0bb730614e
rhysdh540 <rhys.dehaan@me.com>
2023-12-01 16:56:35 -0500

    improve day 1 totally not stolen from https://github.com/CodeF53

--- Year2023/src/Day1.java
+++ Year2023/src/Day1.java
@@ -1,108 +1,35 @@
 import java.util.List;
 
+/**
+ * <a href="https://adventofcode.com/2023/day/1">Day 1</a>
+ */
 @SuppressWarnings({"unused", "unchecked"})
 public class Day1 implements Day {
-	static final String start = "(zero|one|two|three|four|five|six|seven|eight|nine).*";
-	static final String end = ".*(zero|one|two|three|four|five|six|seven|eight|nine)";
 
 	@Override
 	public Integer run1() throws Exception {
-		List<String> input = Main.getInput(1);
-		int sum = 0;
-		input.replaceAll(s -> s.replaceAll("[^0-9]", ""));
-		for(String s : input) {
-			sum += Integer.parseInt(s.charAt(0) + "" + s.charAt(s.length() - 1));
-		}
-
-		return sum;
+		return doTheThing(Main.getInput(1));
 	}
 
 	@Override
 	public Integer run2() throws Exception {
-		List<String> input = Main.getInput(1);
-		int sum = 0;
-
-		for(String in : input) {
-			int[] indices = findFirstAndLastNumberIndices(in);
-
-			String firstNum = in.substring(indices[0]);
-			String secondNum = in.substring(0, indices[1] + 1);
-			String result = "";
-
-			result += getNumber(firstNum, true);
-			result += getNumber(secondNum, false);
-
-			sum += Integer.parseInt(result);
+		List<String> input = Main.getInput(1),
+		// technically we shoudn't include zero but it works anyway
+				nums = List.of("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
+		for(String num : nums) {
+			input.replaceAll(s -> {
+				// pad replacements with their start and end characters so you don't mess up things like `fiveightwone`
+				String toReplace = num.charAt(0) + String.valueOf(nums.indexOf(num)) + num.charAt(num.length() - 1);
+				return s.replaceAll(num, toReplace);
+			});
 		}
-
-		return sum;
+		return doTheThing(input);
 	}
 
-	private int[] findFirstAndLastNumberIndices(String in) {
-		int first = 0, second = 0;
-		for(int i = 0; i < in.length(); i++) {
-			if(isNumberStart(in.substring(i))) {
-				first = i;
-				break;
-			}
-		}
-		for(int i = in.length() - 1; i >= 0; i--) {
-			if(isNumberEnd(in.substring(0, i + 1))) {
-				second = i;
-				break;
-			}
-		}
-		return new int[]{first, second};
-	}
-
-	private String getNumber(String num, boolean isStart) {
-		char c = num.charAt(isStart ? 0 : num.length() - 1);
-		if(Character.isDigit(c)) {
-			return String.valueOf(c);
-		} else {
-			return String.valueOf(isStart ? getNumberStart(num) : getNumberEnd(num));
-		}
-	}
-
-	private static boolean isNumberStart(String s) {
-		char c = s.charAt(0);
-		return c >= '0' && c <= '9' || s.matches(start);
-	}
-
-	private static boolean isNumberEnd(String s) {
-		char c = s.charAt(s.length() - 1);
-		return c >= '0' && c <= '9' || s.matches(end);
-	}
-
-	private static int mapStringToNumber(String s) {
-		return switch(s) {
-	        case "one" -> 1;
-	        case "two" -> 2;
-	        case "thr", "ree" -> 3;
-	        case "fou", "our" -> 4;
-	        case "fiv", "ive" -> 5;
-	        case "six" -> 6;
-	        case "sev", "ven" -> 7;
-	        case "eig", "ght" -> 8;
-	        case "nin", "ine" -> 9;
-	        case "zer", "ero" -> 0;
-	        default -> throw new AssertionError();
-	    };
-	}
-
-	private static int getNumberStart(String s) {
-	    if(s.matches(start)) {
-	        return mapStringToNumber(s.substring(0, 3));
-	    }
-	    System.out.println(s);
-	    throw new AssertionError();
-	}
-
-	private static int getNumberEnd(String s) {
-	    if(s.matches(end)) {
-	        return mapStringToNumber(s.substring(s.length() - 3));
-	    }
-	    System.out.println(s);
-	    throw new AssertionError();
+	private int doTheThing(List<String> input) {
+		return input.stream()
+				.map(s -> s.replaceAll("[^0-9]", ""))
+				.mapToInt(s -> Integer.parseInt(s.charAt(0) + "" + s.charAt(s.length() - 1)))
+				.sum();
 	}
 }

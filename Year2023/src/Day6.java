51c026293b6d0a6305001e40558b544605658d51
Rhys <98863820+rhysdh540@users.noreply.github.com>
2023-12-06 10:34:39 -0500

    optimise day 6 a little more

--- Year2023/src/Day6.java
+++ Year2023/src/Day6.java
@@ -13,7 +13,11 @@ public class Day6 implements Day<Long> {
         String[] distStr = input.get(1).substring(9).trim().split(" +");
         long[] distances = Arrays.stream(distStr).mapToLong(Long::parseLong).toArray();
 
-        return calculate(times, distances);
+        long result = 1;
+        for(int i = 0; i < times.length; i++) {
+            result *= calculate(times[i], distances[i]);
+        }
+        return result;
     }
 
     @Override
@@ -21,28 +25,22 @@ public class Day6 implements Day<Long> {
         List<String> input = Main.getInput(6);
         long time = Long.parseLong(input.get(0).substring(5).replace(" ", ""));
         long distance = Long.parseLong(input.get(1).substring(9).replace(" ", ""));
-        return calculate(new long[] {time}, new long[] {distance});
+        return calculate(time, distance);
     }
 
-    private long calculate(long[] times, long[] distances) {
-        long result = 1;
-        for(int i = 0; i < times.length; i++) {
-            long time = times[i];
-            long distance = distances[i];
-            long first = 0;
-            for(long j = 1; j < time; j++) {
-                if(j * (time - j) > distance) {
-                    first = j;
-                    break;
-                }
-            }
-            for(long j = time - 1; j > first; j--) {
-                if(j * (time - j) > distance) {
-                    result *= (j - first + 1);
-                    break;
-                }
+    private long calculate(long time, long distance) {
+        long first = 0;
+        for(long j = 1; j < time; j++) {
+            if(j * (time - j) > distance) {
+                first = j;
+                break;
             }
         }
-        return result;
+        for(long j = time - 1; j > first; j--) {
+            if(j * (time - j) > distance) {
+                return j - first + 1;
+            }
+        }
+        return 1;
     }
 }

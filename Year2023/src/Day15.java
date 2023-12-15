18fc0d367fb65d625fd6b7b0fa2afdff81ac00fb
Rhys <98863820+rhysdh540@users.noreply.github.com>
2023-12-15 09:53:34 -0500

    Day 15 but slightly better

--- Year2023/src/Day15.java
+++ Year2023/src/Day15.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map.Entry;
 
 /**
 * <a href="https://adventofcode.com/2023/day/15">Day 15</a>
@@ -22,34 +21,33 @@ public class Day15 extends IntDay {
 	}
 
 	@Override
-	public int run2Int(List<String> a) {
-		String[] input = Utils.split(a.get(0), ',');
+	public int run2Int(List<String> input) {
+		String[] insns = Utils.split(input.get(0), ',');
 
 		@SuppressWarnings("unchecked")
 		LinkedHashMap<String, Integer>[] boxes = new LinkedHashMap[256];
 		Arrays.setAll(boxes, i -> new LinkedHashMap<>());
 
-		for(String insn : input) {
+		for(String insn : insns) {
 			boolean remove = insn.contains("-");
 			int index = remove ? insn.indexOf("-") : insn.indexOf("=");
 			String key = insn.substring(0, index);
 
 			LinkedHashMap<String, Integer> box = boxes[HASH(key)];
-			if(insn.contains("-")) {
+			if(remove) {
 				box.remove(key);
 			} else {
-				box.put(key, Integer.parseInt(insn.substring(index + 1)));
+				box.put(key, Utils.fastParseInt(insn.charAt(insn.length() - 1)));
 			}
 		}
 
 		int sum = 0;
 		for(int i = 0; i < 256; i++) {
 			int box = i + 1;
-			Iterator<Entry<String, Integer>> it = boxes[i].entrySet().iterator();
+			Iterator<Integer> it = boxes[i].values().iterator();
 			int index = 1;
 			while(it.hasNext()) {
-				Entry<String, Integer> entry = it.next();
-				sum += (box * index++ * entry.getValue());
+				sum += (box * index++ * it.next());
 			}
 		}
 		return sum;

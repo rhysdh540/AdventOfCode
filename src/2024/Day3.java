274b64c3b75d34c2b425a7e4ae9c695d745e2314
Rhys <98863820+rhysdh540@users.noreply.github.com>
2024-12-03 18:03:14 -0500

    use streams correctly

--- src/2024/Day3.java
+++ src/2024/Day3.java
@@ -3,29 +3,19 @@ import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Pattern;
 
 public class Day3 {
 	public static Object part1(String input) throws Throwable {
-		Pattern mul = Pattern.compile("mul\\((\\d+),(\\d+)\\)");
-
-		AtomicInteger sum = new AtomicInteger();
-		mul.matcher(input).results().forEach(m -> {
-			int a = Integer.parseInt(m.group(1));
-			int b = Integer.parseInt(m.group(2));
-			sum.addAndGet(a * b);
-		});
-
-		return sum;
+		return Pattern.compile("mul\\((\\d+),(\\d+)\\)").matcher(input).results()
+				.mapToInt(m -> Integer.parseInt(m.group(1)) * Integer.parseInt(m.group(2))).sum();
 	}
 
 	public static Object part2(String input) throws Throwable {
 		Pattern pattern = Pattern.compile("(mul|do|don't)\\((?:(\\d+),(\\d+))?\\)");
 
 		AtomicBoolean enabled = new AtomicBoolean(true);
-		AtomicInteger sum = new AtomicInteger();
-		pattern.matcher(input).results().forEach(m -> {
+		return pattern.matcher(input).results().mapToInt(m -> {
 			switch(m.group(1)) {
 				case "do" -> enabled.set(true);
 				case "don't" -> enabled.set(false);
@@ -33,13 +23,13 @@ public class Day3 {
 					if(enabled.get()) {
 						int a = Integer.parseInt(m.group(2));
 						int b = Integer.parseInt(m.group(3));
-						sum.addAndGet(a * b);
+						return a * b;
 					}
 				}
 			}
-		});
 
-		return sum;
+			return 0;
+		}).sum();
 	}
 
 	public static void main(String[] args) throws Throwable {

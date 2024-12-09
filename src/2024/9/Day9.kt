e8689b441760eb5c952ba773362b99957e6d6b04
Rhys <98863820+rhysdh540@users.noreply.github.com>
2024-12-09 07:46:15 -0500

    today so hard

---
+++ src/2024/9/Day9.kt
@@ -0,0 +1,121 @@
+import kotlin.io.path.Path
+import kotlin.io.path.readText
+
+fun part1_9(input: String): Any? {
+    val blocks = mutableListOf<Int>()
+    for ((index, char) in input.withIndex()) {
+        val count = char.digitToInt()
+        if (index % 2 == 0) {
+            repeat(count) { blocks.add(index / 2) }
+        } else {
+            repeat(count) { blocks.add(-1) }
+        }
+    }
+
+    while (blocks.contains(-1)) {
+        val empty = blocks.indexOf(-1)
+        val block = blocks.indexOfLast { it != -1 }
+        blocks[empty] = blocks[block]
+        blocks[block] = -1
+
+        while (blocks.last() == -1) {
+            blocks.removeAt(blocks.lastIndex)
+        }
+    }
+
+    var checksum = 0L
+    for ((position, block) in blocks.withIndex()) {
+        if (block != -1) {
+            checksum += position * block
+        }
+    }
+
+    return checksum
+}
+
+fun part2_9(input: String): Any? {
+    val blocks = mutableListOf<Int>()
+    for ((index, char) in input.withIndex()) {
+        val count = char.digitToInt()
+        if (index % 2 == 0) {
+            repeat(count) { blocks.add(index / 2) }
+        } else {
+            repeat(count) { blocks.add(-1) }
+        }
+    }
+
+    fun fs(blocks: List<Int>): List<Pair<Int, Int>> {
+        val fs = mutableListOf<Pair<Int, Int>>()
+        var start = -1
+        for (i in blocks.indices) {
+            if (blocks[i] == -1) {
+                if (start == -1) start = i
+            } else {
+                if (start != -1) {
+                    fs.add(Pair(start, i - 1))
+                    start = -1
+                }
+            }
+        }
+        if (start != -1) {
+            fs.add(Pair(start, blocks.lastIndex))
+        }
+        return fs
+    }
+
+    data class File(val id: Int, var start: Int, var end: Int, val len: Int)
+
+    val files = (0..input.length / 2).map<Int, File> { id ->
+        val start = blocks.indexOf(id)
+        val end = blocks.lastIndexOf(id)
+        File(id, start, end, (end - start + 1))
+    }.sortedByDescending { it.id }.toMutableList()
+
+    for (file in files) {
+        val fs = fs(blocks)
+        val span = fs.find { span ->
+            span.first + file.len - 1 <= span.second && span.second < file.start
+        }
+        if (span != null) {
+            val newStart = span.first
+            val newEnd = newStart + file.len - 1
+
+            for (i in 0 until file.len) {
+                blocks[newStart + i] = file.id
+            }
+
+            for (i in file.start..file.end) {
+                blocks[i] = -1
+            }
+
+            file.start = newStart
+            file.end = newEnd
+        }
+    }
+
+    var checksum = 0L
+    for ((pos, block) in blocks.withIndex()) {
+        if (block != -1) {
+            checksum += pos * block
+        }
+    }
+
+    return checksum
+}
+
+fun main() {
+    val input = Path("inputs/2024/9.txt").readText()
+
+    var start = System.nanoTime()
+    var result = part1_9(input)
+    var end = System.nanoTime()
+    println("--- Part 1: %.2fms ---".format((end - start) / 1e6))
+    println(result)
+
+    start = System.nanoTime()
+    result = part2_9(input)
+    end = System.nanoTime()
+    println("--- Part 2: %.2fms ---".format((end - start) / 1e6))
+    println(result)
+    println("----------------------")
+}

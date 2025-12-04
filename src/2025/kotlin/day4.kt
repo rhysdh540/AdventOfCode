8fffe600c1c540886d67644044dfe7c93fba409c
Rhys <98863820+rhysdh540@users.noreply.github.com>
2025-12-04 00:26:45 -0500

    bool

--- src/2025/kotlin/day4.kt
+++ src/2025/kotlin/day4.kt
@@ -1,32 +1,32 @@
 import dev.rdh.aoc.*
 
 private fun PuzzleInput.part1(): Any? {
-    return reachable(charGrid).size
+    return reachable(boolGrid('@')).size
 }
 
 private fun PuzzleInput.part2(): Any? {
-    val grid = charGrid.deepToMutableList()
+    val grid = boolGrid('@').deepToMutableList()
     var removed = 0
     while (true) {
         val toRemove = reachable(grid)
         if (toRemove.isEmpty()) break
         for (pos in toRemove) {
-            grid[pos] = '.'
+            grid[pos] = false
         }
         removed += toRemove.size
     }
     return removed
 }
 
-private fun reachable(grid: List<List<Char>>): Set<Pair<Int, Int>> {
+private fun reachable(grid: List<List<Boolean>>): Set<Pair<Int, Int>> {
     val reachable = mutableSetOf<Pair<Int, Int>>()
     for (y in grid.indices) {
         for (x in grid[y].indices) {
-            if (grid[y][x] != '@') continue
+            if (!grid[y][x]) continue
             val around = Vectors.d8.map { it + v(x, y) }
 
             val nrolls = around.count { (ax, ay) ->
-                ay in grid.indices && ax in grid[ay].indices && grid[ay][ax] == '@'
+                ay in grid.indices && ax in grid[ay].indices && grid[ay][ax]
             }
 
             if (nrolls < 4) {

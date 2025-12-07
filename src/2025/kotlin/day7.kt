1163f96d9ac04ddb194c89560f0e3a95accf6374
Rhys <98863820+rhysdh540@users.noreply.github.com>
2025-12-07 14:43:37 -0500

    single pass 2025/7 much smarter approach

--- src/2025/kotlin/day7.kt
+++ src/2025/kotlin/day7.kt
@@ -1,83 +1,37 @@
 import dev.rdh.aoc.*
 
 private fun PuzzleInput.part1(): Any? {
-    val grid = grid
-    val start = run {
-        var s = v(0, 0)
-        for (y in grid.indices) {
-            for (x in grid[y].indices) {
-                if (grid[y][x] == 'S') {
-                    s = v(x, y)
-                }
-            }
-        }
-        s
-    }
-
-    val queue = ArrayDeque<Vec2i>()
-    val found = mutableSetOf<Vec2i>()
-    val visited = mutableSetOf<Vec2i>()
-    queue += start + v(0, 1)
-    while (queue.isNotEmpty()) {
-        val pos = queue.removeFirst()
-        if (pos in visited) continue
-        visited += pos
-        val (x, y) = pos
-        val cell = grid[pos]
-        when (cell) {
-            '^' -> {
-                found += pos
-                if (x > 0) {
-                    queue += v(x - 1, y)
-                }
-                if (x < grid[y].lastIndex) {
-                    queue += v(x + 1, y)
-                }
-            }
-            '.' -> {
-                if (y < grid.lastIndex) {
-                    queue += v(x, y + 1)
-                }
-            }
-        }
-    }
-
-    return found.size
+    return run().first
 }
 
-private fun PuzzleInput.part2(): Any {
-    val grid = grid
-    val start = run {
-        var s = v(0, 0)
-        for (y in grid.indices) {
-            for (x in grid[y].indices) {
-                if (grid[y][x] == 'S') {
-                    s = v(x, y)
+private fun PuzzleInput.part2(): Any? {
+    return run().second
+}
+
+fun PuzzleInput.run(): Pair<Int, Long> {
+    var state = LongArray(grid[0].size)
+    state[grid[0].indexOf('S')] = 1
+    var splits = 0
+
+    for (y in grid.indices) {
+        if (y == 0) continue
+        val next = LongArray(state.size)
+        for ((x, v) in state.withIndex()) {
+            if (v == 0L) continue
+
+            when (grid[x, y]) {
+                '.' -> next[x] += v
+                '^' -> {
+                    splits++
+                    if (x > 0) next[x - 1] += v
+                    if (x < state.lastIndex) next[x + 1] += v
                 }
             }
         }
-        s
+        state = next
     }
 
-    val cache = mutableMapOf<Vec2i, Long>()
-    fun timelines(pos: Vec2i): Long {
-        if (pos in cache) return cache[pos]!!
-
-        if (pos.y !in grid.indices || pos.x !in grid[pos.y].indices) {
-            return 1L
-        }
-
-        val result = when (grid[pos]) {
-            '.' -> timelines(pos + v(0, 1))
-            '^' -> timelines(pos - v(1, 0)) + timelines(pos + v(1, 0))
-            else -> error("Unreachable")
-        }
-
-        cache[pos] = result
-        return result
-    }
-
-    return timelines(start + v(0, 1))
+    return splits to state.sum()
 }
 
 fun main() = PuzzleInput(2025, 7).withSolutions({ part1() }, { part2() }).run()

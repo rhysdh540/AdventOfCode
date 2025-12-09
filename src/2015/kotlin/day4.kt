d9fd7cadb3c0169f478ecb5d7ed6625eee1a48b8
Rhys <98863820+rhysdh540@users.noreply.github.com>
2025-12-08 19:43:21 -0500

    simplify 2015/4

--- src/2015/kotlin/day4.kt
+++ src/2015/kotlin/day4.kt
@@ -1,51 +1,30 @@
 import dev.rdh.aoc.*
 import java.security.MessageDigest
 import java.util.stream.IntStream
+import kotlin.experimental.and
 
 private fun PuzzleInput.part1(): Any? {
-    return run("00000")
+    return run {
+        it[0] == 0.toByte() && it[1] == 0.toByte() && (it[2] and 0xF0.toByte()) == 0.toByte()
+    }
 }
 
 private fun PuzzleInput.part2(): Any? {
-    return run("000000", parallel = true)
+    return run(parallel = true) {
+        it[0] == 0.toByte() && it[1] == 0.toByte() && it[2] == 0.toByte()
+    }
 }
 
-private fun PuzzleInput.run(match: String, parallel: Boolean = false): Int {
+private inline fun PuzzleInput.run(parallel: Boolean = false, crossinline condition: (ByteArray) -> Boolean): Int {
+    val inputBytes = input.toByteArray()
     val md5 = ThreadLocal.withInitial { MessageDigest.getInstance("MD5") }
-
-    val stream = IntStream.iterate(0) { it + 1 }.filter {
-        md5.get().digest((input + it).toByteArray()).hexMatches(match)
-    }
-
-    if (parallel) {
-        stream.parallel()
-    }
-
-    return stream.findFirst().asInt
+    return IntStream.iterate(0, Int::inc).let { if (parallel) it.parallel() else it }.filter { n ->
+        md5.get().apply {
+            reset()
+            update(inputBytes)
+            update(n.toString().toByteArray())
+        }.let { condition(it.digest()) }
+    }.findFirst().asInt
 }
 
-private fun ByteArray.hexMatches(match: String): Boolean {
-    val hexChars = "0123456789abcdef".toCharArray()
-    val len = match.length
-
-    for (i in 0 until len) {
-        val byteIndex = i / 2
-        if (byteIndex >= this.size) {
-            return false
-        }
-        val byte = this[byteIndex].toInt() and 0xFF
-
-        val expectedChar = if (i and 1 == 0) {
-            hexChars[byte ushr 4]
-        } else {
-            hexChars[byte and 0x0F]
-        }
-
-        if (expectedChar != match[i]) {
-            return false
-        }
-    }
-    return true
-}
-
-fun main() = PuzzleInput(2015, 4).withSolutions({ part1() }, { part2() }).run()
+fun main() = PuzzleInput(2015, 4).withSolutions({ part1() }, { part2() }).benchmark()
